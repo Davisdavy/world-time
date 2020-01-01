772f0d5a4c5150512925409fefc35056d6995c6b
Davisdavy <davisdavy96@gmail.com>
2020-01-01 16:52:32 +0300

    Integrate worldtime api

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -9,18 +9,30 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
-   Response response = await get('https://jsonplaceholder.typicode.com/posts/1');
-   Map data = jsonDecode(response.body);
-   print(data);
-   print(data['title']);
+  void getTime() async {
 
+    //make request
+    Response response = await get('http://worldtimeapi.org/api/timezone/Africa/Nairobi');
+    Map data  = jsonDecode(response.body);
+//    print (data);
+
+    //get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    //print(datetime);
+    //print(offset);
+
+    //create a datetime object
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset) ));
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
 
   }
 
